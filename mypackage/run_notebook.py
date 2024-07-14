bd901037a48b5198c25be2d75f94de608b92aaf8
Arunava-27 <drarunkundu22@gmail.com>
2024-07-14 14:06:23 +0530

    run notebook error catch

--- mypackage/run_notebook.py
+++ mypackage/run_notebook.py
@@ -1,7 +1,10 @@
 import subprocess
 
 def main():
-    subprocess.run(["jupyter", "nbconvert", "--to", "notebook", "--execute", "poc_model_ver_2.ipynb"])
+    try:
+        subprocess.run(["jupyter", "nbconvert", "--to", "notebook", "--execute", "poc_model_ver_2.ipynb"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while executing the notebook: {e}")
 
 if __name__ == "__main__":
     main()
